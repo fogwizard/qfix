4c6bad2c1768c84e0989c4d814e5c14d2671fa3f
fogwizard <jwf1990@126.com>
2021-01-08 13:35:32 +0800

    improve qfix
    Change-Id: I7e9cb7d7417419dad62057614b13b3c5914dbaca

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
 #include <fstream>
-#include <string>
+#include <iostream>
 #include <memory>
-#include <vector>
+#include <string>
 #include <tuple>
+#include <vector>
 
 #if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include)
 #if __has_include(<filesystem>)
@@ -20,52 +20,82 @@ namespace fs = ghc::filesystem;
 
 using err_msg_t = std::tuple<std::string, int>;
 
-class Runner {
+class Runner
+{
 public:
-	virtual void run(void) = 0;
-	std::string mFname;
-	int mLine;
-	virtual ~Runner(){};
+    virtual void run(void) = 0;
+    virtual ~Runner() {};
 };
 
-class errorRunner: public Runner {
+class errorRunner : public Runner
+{
 public:
-	errorRunner(const char *logFile) {
-            if(fs::is_regular_file(logFile)) {
-                std::shared_ptr<std::ifstream> inf(new std::ifstream(), [](std::ifstream *fp) {
-			if(fp->is_open()) {
-			    fp->close();
-			}
-		});
+    errorRunner(const char *logFile)
+    {
+        if (fs::is_regular_file(logFile)) {
+            std::shared_ptr<std::ifstream> inf(new std::ifstream(),[](std::ifstream *fp) {
+                if (fp->is_open()) {
+                    fp->close();
+                }
+            });
 
-		fmt::print("input file= {0}\n", logFile);
-                inf->open(logFile, std::ios::in);
-		std::string line;
-		auto count = 0;
-		while(std::getline(*inf, line)) {
-			if (std::string::npos == line.find("error:")) {
-				continue;
-			}
-			errDic.push_back(err_msg_t(line, 100));
-			count++;
-		}
-		fmt::print("total error={0}\n", count);
-            } else {
-                fmt::print("file {0} not exist\n", logFile);
+            fmt::print("input file= {0}\n", logFile);
+            inf->open(logFile, std::ios::in);
+            std::string line;
+
+            while (std::getline(*inf, line)) {
+                if (std::string::npos == line.find("error:")) {
+                    continue;
+                }
+                std::vector<std::string> v = split(line, ":");
+                errDic.push_back(err_msg_t(v[1], std::atoi(v[2].c_str())));
             }
-	}
-	void run(void) {
-           //auto choose = 0;
-           //system(fmt::format("vim {0} +{1}", mFname, mLine).c_str());
-	}
-	void dump(void) {
-		for_each(errDic.begin(), errDic.end(), [](err_msg_t fp){
-                    fmt::print("{0} +{1}\n",std::get<0>(fp), std::get<1>(fp));
-		});
-	}
-	virtual ~errorRunner(){};
+            fmt::print("total error={0}\n", total_error = errDic.size());
+        } else {
+            fmt::print("file {0} not exist\n", logFile);
+        }
+    }
+    std::vector<std::string> split(std::string str, std::string pattern)
+    {
+        std::string::size_type pos;
+        std::vector<std::string> result;
+        str += pattern; //??????????
+        int size = str.size();
+        for (int i = 0; i < size; i++) {
+            pos = str.find(pattern, i);
+            if (pos < size) {
+                std::string s = str.substr(i, pos - i);
+                result.push_back(s);
+                i = pos + pattern.size() - 1;
+            }
+        }
+        return result;
+    }
+    void run(void)
+    {
+        if (0 == total_error) {
+            fmt::print("qfix: build successful\n");
+            return;
+        }
+        for_each(errDic.begin(), errDic.end(), [](err_msg_t fp) {
+            fmt::print("{0} +{1}\n", std::get<0>(fp), std::get<1>(fp));
+        });
+        fmt::print("please choose:\n");
+        auto choose = 0;
+        std::cin >> choose;
+        if (choose > errDic.size()) {
+            fmt::print("input out of range\n");
+            return;
+        }
+        system(fmt::format("vim {0} +{1}", std::get<0>(errDic[choose]),
+                           std::get<1>(errDic[choose]))
+               .c_str());
+    }
+    virtual ~errorRunner() {};
+
 private:
-	std::vector<err_msg_t> errDic;
+    std::vector<err_msg_t> errDic;
+    int total_error;
 };
 
 int usage(const char *name)
@@ -74,15 +104,12 @@ int usage(const char *name)
     return 0;
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     if (2 != argc) {
-	return usage(argv[0]);
+        return usage(argv[0]);
     }
     auto app = std::make_unique<errorRunner>(argv[1]);
     app->run();
-    app->dump();
-
     return 0;
 }
-
